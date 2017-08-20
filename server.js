90352a0bf0d036846f945df299ab724917471619
Austin Alexander Lee <austin.alexander.lee@gmail.com>
2017-08-20 12:34:02 -0700

    adding changes to server.js for db connections, ES6 notations for dependencies and socket io package

--- server.js
+++ server.js
@@ -1,50 +1,60 @@
 // Dependencies
-var express = require('express');
-var bodyParser = require('body-parser');
-var logger = require('morgan');
-var mongoose = require('mongoose');
+const express = require('express');
+const bodyParser = require('body-parser');
+const logger = require('morgan');
+const mongoose = require('mongoose');
 
-var Article = require('./models/Article.js');
-//var request = require('request');
-
-mongoose.Promise = Promise;
+const app = express();
 var PORT = process.env.PORT || 3000;
 
-var app = express();
-var router = express.Router();
-require("./app/config/routes")(router);
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
 
 app.use(logger("dev"));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
-  extended: true
+  extended: false
 }));
-app.use(bodyParser.text());
-app.use(bodyParser.json({type:"application/vnd.api+json"}));
+
 app.use(express.static("public"));
 
-//var routes = require("./app/config/routes.js");
-//app.use("/", routes);
-//mongoose.connect("mongodb://heroku_gnzk5747:4d2121nhgnfbdl1pfirsdepk9n@ds125262.mlab.com:25262/heroku_gnzk5747");
-
+// database configuration with mongoose
+//--------------------------------------
+var databaseUri = "mongod://localhost/nytreact";
 if (process.env.MONGODB_URI) {
-  
+  mongoose.connect(process.env.MONGODB_URI);
+} else {
+  mongoose.connect(databaseUri);
 }
 
-mongoose.connect('mongodb://localhost/nytreact');
-
-
-
+// initialize connection to mongoose database
 var db = mongoose.connection;
 
-// db.on("error", function(error) {
-//   console.log("Mongoose Error: ", error);
-// });
-//
-// db.once("open", function() {
-//   console.log("Mongoose connection successful.");
-// });
+db.on('error', function(err) {
+  console.log('Mongoose Error: ', err);
+});
 
+db.once('open', function() {
+  console.log('Mongoose connection successful.');
+});
+
+io.on('connection', function(socket) {
+  console.log("The user is connected.");
+  socket.on('This article is added.', function(data) {
+    io.emit('This article is added.');
+  });
+});
+
+// importing the use of routes and controllers
+var Article = require('./models/Article.js');
+var router = require("./app/config/routes")(router);
+
+
+
+// app.use(bodyParser.text());
+// app.use(bodyParser.json({type:"application/vnd.api+json"}));
+
+app.use('/', router);
 app.listen(PORT, function() {
   console.log("App running on PORT " + PORT);
 });
