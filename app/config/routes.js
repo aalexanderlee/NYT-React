e4baa8bc6515545bde4a627e1b81a31ae0387494
Austin Alexander Lee <austin.alexander.lee@gmail.com>
2017-08-20 17:59:27 -0700

    package Browser Router is included into routes JS to allow client and server to use similar router

--- app/config/routes.js
+++ app/config/routes.js
@@ -1,4 +1,19 @@
 // list dependencies
 var React = require("react");
 var router = require("react-router-dom");
+
+// individual routes componenent included
 var Route = router.Route;
+// pass in configurations as props
+var BrowserRouter = router.BrowserRouter;
+
+var Main = require("../components/Main");
+
+// allows you to abstract routing
+// client and server will operate with similar router
+// when the server has not been built
+module.exports = (
+  <BrowserRouter>
+    <Route component = {Main} />
+  </BrowserRouter>
+);
